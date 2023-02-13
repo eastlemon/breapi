db3894d93d76d05cd779b5390ab6f25b121816f0
rest32105 <rest32105@gmail.com>
2023-02-13 22:54:40 +0700

    2.6 Filler by Redis Driver Fix15

--- jobs/FillerJob.php
+++ jobs/FillerJob.php
@@ -23,20 +23,20 @@ class FillerJob extends BaseObject implements JobInterface
         try {
             if (isset($this->data[$this->keys['inn']])) {
                 if ($_inn = Inn::findOne(['inn' => $this->data[$this->keys['inn']]])) {
-            Yii::error($this->keys['inn']);
-            Yii::error($this->data[$this->keys['inn']]);
+                    Yii::error($this->keys['inn']);
+                    Yii::error($this->data[$this->keys['inn']]);
                     Yii::error($_inn);
-                    $fios = end($_inn->fios);
-                    Yii::error($fios->fio);
-                    $phones = end($_inn->phones);
-                    Yii::error($phones->phone);
-
-                    $filler = new Filler();
-                    $filler->id_file = $this->fid;
-                    $filler->fio = end($_inn->fios)->fio;
-                    $filler->inn = $_inn->inn;
-                    $filler->phone = (string) $this->data[$this->keys['phone']] ?: end($_inn->phones)->phone;
-                    $filler->save();
+//                    $fios = end($_inn->fios);
+//                    Yii::error($fios->fio);
+//                    $phones = end($_inn->phones);
+//                    Yii::error($phones->phone);
+//
+//                    $filler = new Filler();
+//                    $filler->id_file = $this->fid;
+//                    $filler->fio = end($_inn->fios)->fio;
+//                    $filler->inn = $_inn->inn;
+//                    $filler->phone = (string) $this->data[$this->keys['phone']] ?: end($_inn->phones)->phone;
+//                    $filler->save();
                 }
             }
         } catch (Exception $e) {
