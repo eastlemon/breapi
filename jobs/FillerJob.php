a6d6104a7d907918ac51771af7aeeaf0cd00c535
rest32105 <rest32105@gmail.com>
2023-02-13 23:36:19 +0700

    2.6 Filler by Redis Driver Fix22

--- jobs/FillerJob.php
+++ jobs/FillerJob.php
@@ -29,8 +29,8 @@ class FillerJob extends BaseObject implements JobInterface
             Yii::error(Json::encode($_j));
             Yii::error(Json::encode($_j->fios));
             Yii::error(Json::encode($_j->phones));
-            Yii::error(Json::encode($_j->fios[array_key_last($_j->fios)]));
-            Yii::error(Json::encode($_j->phones[array_key_last($_j->phones)]));
+            Yii::error(Json::encode($_j->fios[array_key_last($_j->fios)]->fio));
+            Yii::error(Json::encode($_j->phones[array_key_last($_j->phones)]->phone));
 //            if ($_inn = Inn::findOne(['inn' => $this->data[$this->keys['inn']]])) {
 //                Yii::error($_inn);
 //                    $fios = end($_inn->fios);
