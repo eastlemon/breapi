1463e0b3ac07bc453c170cc1bdc6f89e72deb953
rest32105 <rest32105@gmail.com>
2023-02-13 22:37:00 +0700

    2.6 Filler by Redis Driver Fix12

--- jobs/FillerJob.php
+++ jobs/FillerJob.php
@@ -4,9 +4,14 @@ namespace app\jobs;
 
 use app\models\Inn;
 use app\modules\admin\models\Filler;
+use Yii;
 use yii\base\BaseObject;
+use yii\base\Exception;
 use yii\queue\JobInterface;
 
+/**
+ * @throws Exception
+ */
 class FillerJob extends BaseObject implements JobInterface
 {
     public array $data;
@@ -15,22 +20,26 @@ class FillerJob extends BaseObject implements JobInterface
 
     public function execute($queue): void
     {
-        \Yii::error('1');
-        if (isset($this->data[$this->keys['inn']])) {
-            if ($_inn = Inn::findOne(['inn' => $this->data[$this->keys['inn']]])) {
-                \Yii::error($_inn);
-                $fios = end($_inn->fios);
-                \Yii::error($fios->fio);
-                $phones = end($_inn->phones);
-                \Yii::error($phones->phone);
+        try {
+            Yii::error('1');
+            if (isset($this->data[$this->keys['inn']])) {
+                if ($_inn = Inn::findOne(['inn' => $this->data[$this->keys['inn']]])) {
+                    Yii::error($_inn);
+                    $fios = end($_inn->fios);
+                    Yii::error($fios->fio);
+                    $phones = end($_inn->phones);
+                    Yii::error($phones->phone);
 
-                $filler = new Filler();
-                $filler->id_file = $this->fid;
-                $filler->fio = end($_inn->fios)->fio;
-                $filler->inn = $_inn->inn;
-                $filler->phone = (string) $this->data[$this->keys['phone']] ?: end($_inn->phones)->phone;
-                $filler->save();
+                    $filler = new Filler();
+                    $filler->id_file = $this->fid;
+                    $filler->fio = end($_inn->fios)->fio;
+                    $filler->inn = $_inn->inn;
+                    $filler->phone = (string) $this->data[$this->keys['phone']] ?: end($_inn->phones)->phone;
+                    $filler->save();
+                }
             }
+        } catch (Exception $e) {
+            Yii::info($e->getMessage(), 'jobs');
         }
     }
 }
